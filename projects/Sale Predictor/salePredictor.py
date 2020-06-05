1095c7df6df58e3262e41f8c8c2541c4907c110d
Alperen Çiçek <alperencicek1998@gmail.com>
2020-06-05 19:50:07 +0300

    05.06.2020UpdateCommit

--- projects/Sale Predictor/salePredictor.py
+++ projects/Sale Predictor/salePredictor.py
@@ -6,9 +6,10 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from PyQt5 import QtWidgets
+from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QFileDialog, QLineEdit, QMainWindow
 import sys
+from PyQt5.QtGui import QFont
 
 class LSVR:
     def __init__(self, csvName):
@@ -35,8 +36,7 @@ class LSVR:
             pro.intercept = modelReg.intercept_
             pro.coef = modelReg.coef_
             pro.formula = str(round(pro.model.params[0], 2)) + " + TV" + " * " + str(round(pro.model.params[1], 2))
-            g = sns.regplot(pro.x, sales.y, ci = None, label = "TV", scatter_kws = {'color' : 'r', 's' : 9})
-            g.set_xlabel("TV Harcamalar")
+            g = sns.regplot(pro.x, sales.y, ci = None, label = "TV", scatter_kws = {'color' : 'b', 's' : 9})
         if radioCheck:
             lm = smf.ols("sales ~ radio", self.dataSet)
             pro.model = lm.fit()
@@ -48,8 +48,7 @@ class LSVR:
             pro.intercept = modelReg.intercept_
             pro.coef = modelReg.coef_
             pro.formula = str(round(pro.model.params[0], 2)) + " + R" + " * " + str(round(pro.model.params[1], 2))
-            g = sns.regplot(pro.x, sales.y, ci = None, label = "Radio", scatter_kws = {'color' : 'b', 's' : 9})
-            g.set_xlabel("Radio Harcamalar")
+            g = sns.regplot(pro.x, sales.y, ci = None, label = "Radio", scatter_kws = {'color' : 'r', 's' : 9})
         if newspaperCheck:
             lm = smf.ols("sales ~ newspaper", self.dataSet)
             pro.model = lm.fit()
@@ -61,22 +60,18 @@ class LSVR:
             pro.intercept = modelReg.intercept_
             pro.coef = modelReg.coef_
             pro.formula = str(round(pro.model.params[0], 2)) + " + N" + " * " + str(round(pro.model.params[1], 2))
-            g = sns.regplot(pro.x, sales.y, ci = None, label = "Newsapaper", scatter_kws = {'color' : 'g', 's' : 9})
-            g.set_xlabel("Newspaper Harcamalar")
+            g = sns.regplot(pro.x, sales.y, ci = None, label = "Newspaper", scatter_kws = {'color' : 'b', 's' : 9})
         if TVCheck == False:
             if radioCheck == False:
                 if newspaperCheck == False:
                     print("There is not graph to view!")
         g.set_ylabel("Satış Sayısı")
+        g.set_xlabel("Harcamalar")
         plt.legend()
         plt.show()
-        
 
     def compare(self):
-        sns.pairplot(self.dataSet, kind = "reg")
-        sns.jointplot(x = "TV", y = "sales", data = self.dataSet, kind = "reg")
-        sns.jointplot(x = "radio", y = "sales", data = self.dataSet, kind = "reg")
-        sns.jointplot(x = "newspaper", y = "sales", data = self.dataSet, kind = "reg")
+        self.selectModel(True, True, True)
 
 class Product:
     def __init__(self):
@@ -100,12 +95,9 @@ class Product:
         else:
             print("No value!")
 
-
     def predict(self, value):#linearRegressionBySklearn
         if self.model is not None:
-            reg = LinearRegression()
-            model = reg.fit(self.x, sales.y)
-            return model.predict([[int(value)]])
+            return float(self.model.params[0]) + (float(value) * float(self.model.params[1]))
         else:
             print("No value to predict!")
 
@@ -121,7 +113,6 @@ class GUI(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        
         self.labelMSE = QtWidgets.QLabel(self)
         self.labelMSE.setText("MSE = ")
         self.labelMSE.move(20, 20)
@@ -161,79 +152,109 @@ class GUI(QMainWindow):
         self.valueFormula = QtWidgets.QLabel(self)
         self.valueFormula.setText("")
         self.valueFormula.move(80, 100)
+  
+        self.textBoxPrediction = QLineEdit(self)
+        self.textBoxPrediction.setPlaceholderText("Predict the value!")
+        self.textBoxPrediction.move(20, 130)
+        self.textBoxPrediction.setEnabled(False)
+
+        self.valuePrediction = QtWidgets.QLabel(self)
+        self.valuePrediction.setText("")
+        self.valuePrediction.move(120, 130)
 
         self.TVButton = QtWidgets.QPushButton(self)
         self.TVButton.setText("TV ~ Sales")
         self.TVButton.move(0, 0)
         self.TVButton.clicked.connect(self.TVButtonClicked)
+        self.TVButton.setEnabled(False)
 
         self.RadioButton = QtWidgets.QPushButton(self)
         self.RadioButton.setText("Radio ~ Sales")
         self.RadioButton.move(100, 0)
         self.RadioButton.clicked.connect(self.RadioButtonClicked)
+        self.RadioButton.setEnabled(False)
 
         self.NewspaperButton = QtWidgets.QPushButton(self)
         self.NewspaperButton.setText("Newspaper ~ Sales")
         self.NewspaperButton.move(200, 0)
         self.NewspaperButton.clicked.connect(self.NewspaperButtonClicked)
+        self.NewspaperButton.setEnabled(False)
 
-        self.GettingCFDButton = QtWidgets.QPushButton(self)
-        self.GettingCFDButton.setText("Open CFD File")
-        self.GettingCFDButton.move(200, 50)
-        self.GettingCFDButton.clicked.connect(self.GettingCFDButtonClicked)
+        self.GettingCSVButton = QtWidgets.QPushButton(self)
+        self.GettingCSVButton.setText("Open CSV File")
+        self.GettingCSVButton.move(200, 70)
+        self.GettingCSVButton.clicked.connect(self.GettingCSVButtonClicked)
+        self.GettingCSVButton.resize(100, 50)
 
         self.PredictButton = QtWidgets.QPushButton(self)
         self.PredictButton.setText("Predict")
         self.PredictButton.move(200, 130)
         self.PredictButton.clicked.connect(self.PredictButtonClicked)
+        self.PredictButton.setEnabled(False)  
 
-        self.predictValue = QLineEdit(self)
-        self.predictValue.setPlaceholderText("Predict the value!")
-        self.predictValue.move(20, 130)
-
-        self.valuePrediction = QtWidgets.QLabel(self)
-        self.valuePrediction.setText("")
-        self.valuePrediction.move(130, 130)
+        self.compareButton = QtWidgets.QPushButton(self)
+        self.compareButton.setText("Compare")
+        self.compareButton.move(200, 35)
+        self.compareButton.clicked.connect(self.CompareButtonClicked)
+        self.compareButton.setEnabled(False)
 
     def TVButtonClicked(self):
-        sample = LSVR(self.CFDName[0])
+        sample = LSVR(self.CSVName[0])
         sample.selectModel(True, False, False)
         self.valueMSE.setText(str(pro.MSE))
         self.valueRMSE.setText(str(pro.RMSE))
         self.valueIntercept.setText(str(pro.intercept))
         self.valueCoef.setText(str(pro.coef))
         self.valueFormula.setText(str(pro.formula))
+        self.PredictButton.setEnabled(True)
+        self.textBoxPrediction.setEnabled(True)
 
     def RadioButtonClicked(self):
-        sample = LSVR(self.CFDName[0])
+        sample = LSVR(self.CSVName[0])
         sample.selectModel(False, True, False)        
         self.valueMSE.setText(str(pro.MSE))
         self.valueRMSE.setText(str(pro.RMSE))
         self.valueIntercept.setText(str(pro.intercept))
         self.valueCoef.setText(str(pro.coef))
         self.valueFormula.setText(str(pro.formula))
-
+        self.PredictButton.setEnabled(True)
+        self.textBoxPrediction.setEnabled(True)
 
     def NewspaperButtonClicked(self):
-        sample = LSVR(self.CFDName[0])
+        sample = LSVR(self.CSVName[0])
         sample.selectModel(False, False, True)        
         self.valueMSE.setText(str(pro.MSE))
         self.valueRMSE.setText(str(pro.RMSE))
         self.valueIntercept.setText(str(pro.intercept))
         self.valueCoef.setText(str(pro.coef))
         self.valueFormula.setText(str(pro.formula))
-
+        self.PredictButton.setEnabled(True)
+        self.textBoxPrediction.setEnabled(True)
 
     def PredictButtonClicked(self):
-        self.valuePrediction.setText(str(pro.predict(self.predictValue.text()[0])))
+        self.valuePrediction.setText(str(pro.predict(self.textBoxPrediction.text())))
 
-    def GettingCFDButtonClicked(self):
-        self.CFDName = QFileDialog.getOpenFileName()
-        print(self.CFDName[0])
+    def GettingCSVButtonClicked(self):
+        self.CSVName = QFileDialog.getOpenFileName()
+        self.TVButton.setEnabled(True)
+        self.RadioButton.setEnabled(True)
+        self.NewspaperButton.setEnabled(True)
+        self.compareButton.setEnabled(True)
+
+    def CompareButtonClicked(self):
+        sample = LSVR(self.CSVName[0])
+        sample.compare()  
+        self.valueMSE.setText("")
+        self.valueRMSE.setText("")
+        self.valueIntercept.setText("")
+        self.valueCoef.setText("")
+        self.valueFormula.setText("")
+        self.PredictButton.setEnabled(False)
+        self.textBoxPrediction.setEnabled(False)
 
 def window():
     app = QApplication(sys.argv)
-    win = GUI(300, 300, 500, 200, "Sale Predictor")
+    win = GUI(300, 300, 300, 180, "Sale Predictor")
     win.show()
     sys.exit(app.exec_())
 
